60b68a93b46df4824885d016ad24b7e15a52c386
Marko Kvakic <mkvakic@srce.hr>
2023-01-20 08:50:31 +0100

    books download

---
+++ download-books.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p books
+nprocs=100
+ndocuments=$(( 10**4 ))
+(
+  for idoc in $( seq 1 $ndocuments )
+  do
+    id=$( printf "%05d" $RANDOM )
+    ((i=i%nprocs)); ((i++==0)) && wait
+    wget -nc -r --accept-regex="$id.*txt" https://www.gutenberg.org/files/$id/ -O books/$id.txt &
+  done
+)
