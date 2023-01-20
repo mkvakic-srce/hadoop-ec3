351de3d4ae0966c2e07cdb14ce433deaed194497
Marko Kvakic <mkvakic@srce.hr>
2023-01-20 15:48:22 +0100

    config files refactor

--- hadoop/etc/hadoop-env.sh
+++ hadoop/etc/hadoop-env.sh
@@ -414,11 +414,11 @@ export HADOOP_OS_TYPE=${HADOOP_OS_TYPE:-$(uname -s)}
 # It uses the format of (command)_(subcommand)_USER.
 #
 # For example, to limit who can execute the namenode command,
-export HDFS_NAMENODE_USER=root
-export HDFS_DATANODE_USER=root
-export HDFS_SECONDARYNAMENODE_USER=root
-export YARN_RESOURCEMANAGER_USER=root
-export YARN_NODEMANAGER_USER=root
+export HDFS_NAMENODE_USER=hdfs
+export HDFS_DATANODE_USER=hdfs
+export HDFS_SECONDARYNAMENODE_USER=hdfs
+export YARN_RESOURCEMANAGER_USER=yarn
+export YARN_NODEMANAGER_USER=yarn
 
 
 ###
